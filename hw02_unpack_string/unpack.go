b233304c3413df424f17d2634ec5d23ef125d3cc
Fochkin <ifochkin@gmail.com>
2024-04-16 13:34:36 +0300

    first attempt

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -7,7 +7,7 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func iterate(input, res string) (string, error) {
-	//if input is empty
+	// if input is empty
 	if len(input) == 0 {
 		return res, nil
 	}
@@ -27,12 +27,10 @@ func iterate(input, res string) (string, error) {
 		return res, nil
 	}
 
-	//counter of recurde calls
+	// counter of recurde calls
 	howMuchSliced := 0
 	// check if next char is digit and not nil
-	//log.Println(char, nextChar, nextChar == 92, input)
 	if (nextChar >= 49 && nextChar <= 57) || (nextChar == 92 && char == 92) {
-
 		if char == 92 {
 			if len(input) >= 3 && (input[2] >= 48 && input[2] <= 57) {
 				for i := 0; i < int(input[2]-48); i++ {
@@ -43,7 +41,6 @@ func iterate(input, res string) (string, error) {
 				res += string(nextChar)
 				howMuchSliced = 2
 			}
-
 		} else {
 			for i := 0; i < int(nextChar-48); i++ {
 				res += string(char)
